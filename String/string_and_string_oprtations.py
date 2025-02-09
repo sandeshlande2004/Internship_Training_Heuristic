c224081a0c9f100dbbcd9323ac336f15ee13c03f
Sandesh Lande <150541370+sandeshlande2004@users.noreply.github.com>
2025-02-09 18:41:16 +0530

    Add files via upload

---
+++ String/string_and_string_oprtations.py
@@ -0,0 +1,10 @@
+str1 = 'Rohit'
+str2 = 'Virat'
+str3 = 'Sachin'
+jno1 = 45
+jno2 = 18
+jno3 = 10
+print(str1 + str2 + str3) #concatenation of strings ie addition
+print(str1+ " " +str(jno1)) #concatenation of string and number
+print(str2+ " " +str(jno2))
+print(str3+ " " +str(jno3))
