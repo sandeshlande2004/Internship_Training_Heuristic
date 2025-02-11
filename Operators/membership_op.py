fccecee5b77164eac0c07040ec202ba539c81286
Sandesh Lande <150541370+sandeshlande2004@users.noreply.github.com>
2025-02-11 20:43:08 +0530

    Add files via upload

---
+++ Operators/membership_op.py
@@ -0,0 +1,13 @@
+#membership operator
+
+print('Membership operators')
+
+str1 = 'Chatgpt, Geminai, Openai and DeepSeek are all the new AI models that are famous and most used in the world'
+print('Chatgpt' in str1)
+print('Geminai' not in str1)
+print('Copilot' in str1)
+
+# Membership operators with list
+list = [154, 'Hitesh', 'Ram', 88.26]
+print(154 in list)
+print('Hitesh' not in list)
