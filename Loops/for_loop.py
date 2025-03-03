532874a52944f41a64a1f46b6188398ca8a81fa0
Sandesh Lande <150541370+sandeshlande2004@users.noreply.github.com>
2025-03-03 18:29:57 +0530

    Add files via upload

---
+++ Loops/for_loop.py
@@ -0,0 +1,12 @@
+# for a in range(2, 22, 2):
+#     print(a)
+    
+# for a in range(6, 10):
+#     print(a)
+
+p = [1, 2, 3]
+q = ['a', 'b', 'c']
+
+for S in  p:
+    for T in  q:
+        print(S, T)
